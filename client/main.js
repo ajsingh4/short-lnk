3805525f90c19dbb2f2c2964e322fb378fee3571
Aj Gill <ajgill@ucdavis.edu>
2019-05-13 17:58:35 -0700

    Route Initialization
    Utilized react-router-dom to set up Link, Login, NotFound, and Signup components as well as their routes.

--- client/main.js
+++ client/main.js
@@ -1,10 +1,24 @@
 import { Meteor } from "meteor/meteor";
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter as Router, Route , Switch} from "react-router-dom";
 
+import Login from "../imports/ui/Login";
 import Signup from "../imports/ui/Signup";
 import Link from "../imports/ui/Link";
+import NotFound from "../imports/ui/NotFound";
+
+const routes = (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Login}/>
+      <Route exact path="/signup" component={Signup}/>
+      <Route exact path="/links" component={Link}/>
+      <Route exact path="*" component={NotFound}/>
+    </Switch>
+  </Router>
+);
 
 Meteor.startup(() => {
-  ReactDOM.render(<Link/>, document.getElementById('app'));
+  ReactDOM.render(routes, document.getElementById("app"));
 });
