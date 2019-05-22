773c4f5e01c6b3355057ff6ef3c650ee9953ded9
Aj Gill <ajgill@ucdavis.edu>
2019-05-22 15:07:09 -0700

    Logout functionality and page authentication
    Set up a logout function in the Link component and set up user authentication checking in main.js. If user is logged in, will only be given access to authorized pages and vice versa for not logged in users.

--- client/main.js
+++ client/main.js
@@ -1,17 +1,24 @@
-import { Meteor } from "meteor/meteor";
+import {Meteor} from "meteor/meteor";
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {Router, Route, Switch} from "react-router";
+import {Tracker} from "meteor/tracker";
+import {createBrowserHistory} from "history";
 
 import Login from "../imports/ui/Login";
 import Signup from "../imports/ui/Signup";
 import Link from "../imports/ui/Link";
 import NotFound from "../imports/ui/NotFound";
 
-//window.browserHistory = this.props.history;
+const browserHistory = createBrowserHistory();
+window.browserHistory = browserHistory;
+let bhistory = window.browserHistory;
+
+const unauthenticatedPages = ["/", "/signup"];
+const authenticatedPages = ["/links"];
 
 const routes = (
-  <Router>
+  <Router history = {browserHistory}>
     <div>
       <Switch>
         <Route exact path="/" component={Login}/>
@@ -22,6 +29,20 @@ const routes = (
     </div>
   </Router>
 );
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  const pathname = bhistory.location.pathname;
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+  const isAuthenticatedPage = authenticatedPages.includes(pathname);
+
+  if(isAuthenticated == true && isUnauthenticatedPage){
+    browserHistory.push("/links");
+  }
+   else if(isAuthenticated == false && isAuthenticatedPage){
+    browserHistory.push("/");
+  }
+});
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById("app"));
--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,9 +1,10 @@
 import React from "react";
 import {BrowserRouter as Router} from "react-router-dom";
+import {Accounts} from "meteor/accounts-base";
 
 export default class Link extends React.Component {
   onLogout(){
-    this.props.history.push("/");
+    Accounts.logout();
   }
   render() {
     return (
