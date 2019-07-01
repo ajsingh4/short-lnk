70469e0e66be525ffc4f5a9e609ab3310da87409
Aj Gill <ajgill@ucdavis.edu>
2019-07-01 15:59:59 -0700

    Input and Display Links
    Created a basic form to take in links from the user in Link.js. Render a links array in LinksList component which is mapped to the input information from Link.js.

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -2,15 +2,35 @@ import React from "react";
 import {BrowserRouter as Router} from "react-router-dom";
 import {Accounts} from "meteor/accounts-base";
 
+import {Links} from "../api/links";
+import LinksList from "./LinksList";
+
 export default class Link extends React.Component {
   onLogout(){
     Accounts.logout();
+  }
+  onSubmit(e){
+    const url = this.refs.url.value.trim();
+    e.preventDefault();
+
+    if (url) {
+      Links.insert({url});
+      this.refs.url.value = "";
+    }
+
   }
   render() {
     return (
       <div>
         <h1>Links Page</h1>
         <button onClick={this.onLogout.bind(this)}> Logout</button>
+        <LinksList/>
+        <p> Add Link </p>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type = "text" ref="url" placeholder="URL"/>
+          <button> Add Link </button>
+
+        </form>
       </div>
     );
   }
