3a6d38cb4e97374f57874613eb08c486b61dd428
Aj Gill <ajgill@ucdavis.edu>
2019-07-15 12:11:46 -0700

    Modal Error Handling & Visit
    Created an error state to show the error for a non valid link within the modal. Used onAfterOpen to default cursor into the form after opening the add link modal. Used onRequestClose to allow for exiting of the modal by clicking outside of it. Created a visit link so users don't need to copy paste everytime.

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -7,31 +7,44 @@ export default class AddLink extends React.Component{
     super(props);
     this.state = {
       url: "",
-      isOpen:false
+      isOpen:false,
+      error: ""
     };
   }
   onSubmit(e){
     const url = this.state.url;
     e.preventDefault();
-    if (url) {
-      Meteor.call("links.insert", url, (err, res) => {
-        if(!err) {
-          this.setState({url: "", isOpen: false});
-        }
-      });
-    }
+    Meteor.call("links.insert", url, (err, res) => {
+      if(!err) {
+          this.handleModalClose();
+      }
+      else {
+        this.setState({error: err.reason});
+      }
+    });
   }
   onChange(e){
     this.setState({
       url: e.target.value.trim()
     });
   }
+  handleModalClose() {
+    this.setState({
+      isOpen:false,
+      url:"",
+      error: ""
+    });
+  }
   render() {
     return(
       <div>
         <button onClick={() => this.setState({isOpen:true})}>+ Add Link</button>
-        <Modal isOpen={this.state.isOpen} contentLabel="Add Link">
-          <p> Add Link </p>
+        <Modal isOpen={this.state.isOpen}
+          contentLabel="Add Link"
+          onAfterOpen={() => this.refs.url.focus()}
+          onRequestClose = {this.handleModalClose.bind(this)}>
+          <h1> Add Link </h1>
+          {this.state.error ? <p>{this.state.error}</p> : undefined}
           <form onSubmit={this.onSubmit.bind(this)}>
             <input type = "text"
               ref="url"
@@ -40,9 +53,9 @@ export default class AddLink extends React.Component{
               onChange={this.onChange.bind(this)}/>
             <button> Add Link </button>
           </form>
-          <button onClick={() => this.setState({isOpen:false, url:""})}>Cancel</button>
+          <button onClick={this.handleModalClose.bind(this)}>Cancel</button>
         </Modal>
       </div>
     );
-  }
+  };
 }
